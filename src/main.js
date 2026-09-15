bfc1d4012cf22850f9c547d7d31183b71255e9e8
prodanovicjoca10-max <prodanovicjoca10@gmail.com>
2026-09-15 22:23:53 +0200

    Add function exercises

--- src/main.js
+++ src/main.js
@@ -27,4 +27,51 @@ console.log(exoticText);
 console.log(exoticText.length);
 
 
+// Functions E1: Greet Function
+// ========================================
+
+function greet(name) {
+  if (typeof name !== "string") {
+    return null;
+  }
+
+  return `Hello, ${name}!`;
+}
+
+console.log(greet("John"));
+console.log(greet(25));
+
+// Functions E2: Add Function
+// ========================================
+
+function add(number1, number2) {
+  if (typeof number1 !== "number" || typeof number2 !== "number") {
+    return null;
+  }
+
+  return number1 + number2;
+}
+
+console.log(add(5, 3));
+console.log(add("5", "3"));
+
+// Functions E3: Subtract Function
+// ========================================
+
+function subtract(number1, number2) {
+  if (typeof number1 !== "number" || typeof number2 !== "number") {
+    return null;
+  }
+
+  return number1 - number2;
+}
+
+console.log(subtract(5, 3));
+console.log(subtract("5", "3"));
+
+
+
+
+
+
 
